54b7f9bf18854bc8075216922331651d3b75eaa4
Bradley Bowen <bowebr231@gmail.com>
2020-03-06 15:26:05 -0600

    Improved MinHeap's comments, and removed unused Comparable interface code at the class declaration line.

--- src/program1/MinHeap.java
+++ src/program1/MinHeap.java
@@ -2,7 +2,13 @@ package program1;
 
 import java.util.ArrayList;
 
-public class MinHeap <E extends Comparable<E>>
+
+/**
+ * The MinHeap class manages and stores all ProcessNodes. It's functions can also handle calls from threads.
+ * @author Ethan McNabb
+ * @author Bradley Bowen
+ */
+public class MinHeap
 {
     //Need to use ArrayList
     //Modified tree structure : parent is (currentIndex -1 / 2)
@@ -18,38 +24,44 @@ public class MinHeap <E extends Comparable<E>>
         m_heap = new ArrayList<ProcessNode>();
     }
 
-    private void heapify(int heapTop) //top is min value
+    /**
+     * recursively sorts minHeap after a node has been removed from the top so that it maintains heap properties.
+     * @param heapTop the top of the heap and sub-heaps during recursive calls.
+     */
+    private void heapify(int heapTop)
     {
         int leftChildIndex = (heapTop * 2) + 1;
         int rightChildIndex = (heapTop * 2) + 2;
-        int swapIndex;
+        int swapIndex; //Index that has greatest priority compared to heapTop.
 
         //Check to see if 2 children exist
         if(rightChildIndex < m_heap.size() )
         {
             //Find highest priority child (lowest number)
             if (m_heap.get(leftChildIndex).m_priority <= m_heap.get(rightChildIndex).m_priority)
+                //Left child is higher priority.
                 swapIndex = leftChildIndex;
 
             //Right child is higher priority (Lower number)
             else
                 swapIndex = rightChildIndex;
 
-            //Compare highest p child with parent
+            //Compare highest p child with parent. If child is higher p, then swap.
             if (m_heap.get(swapIndex).m_priority < m_heap.get(heapTop).m_priority)
             {
-                //If true then swap and sort.
+                //If true then swap and sort new sub-heap.
                 swap(m_heap, swapIndex, heapTop);
                 heapify(swapIndex);
 
-                //Else, do nothing.
+
             }
+            //Else, do nothing.
 
         }
         //Check if only left child exists
         else if(leftChildIndex < m_heap.size())
         {
-            //Check if Parent is higher priority (lowest number) than left child.
+            //Check if Parent is higher priority (lowest number) than left child. If so, swap.
             if (m_heap.get(leftChildIndex).m_priority <= m_heap.get(heapTop).m_priority)
             {
                 swapIndex = leftChildIndex;
@@ -57,6 +69,7 @@ public class MinHeap <E extends Comparable<E>>
                 heapify(swapIndex);
 
             }
+            //Else, do nothing.
         }
 
         //Else Process is a leaf node and heapify is finished.
@@ -66,14 +79,19 @@ public class MinHeap <E extends Comparable<E>>
 
     }
 
-    private void checkHeap()
+    /**
+     * debugging function. Prints the heap to see its current state.
+     *
+     */
+    private void debugPrintHeap()
     {
-        //Check that all parents are smaller than children.
+        System.out.print("Heap: ");
+
         for(int i = 0; i < m_heap.size(); i++)
         {
             System.out.print(m_heap.get(i).m_priority);
         }
-        System.out.println("\n");
+        System.out.print("\n\n");
     }
 
     /**
@@ -99,20 +117,23 @@ public class MinHeap <E extends Comparable<E>>
 
 
 
-
+    /**
+     * Removes top node from min-heap, and then calls heapify to sort min-heap.
+     * @return highest priority (lowest number) Process from heap OR null if empty.
+     */
     public ProcessNode removeProcessFromHeap()
     {
         synchronized (lock)
         {
-            //remove process from the arrayList and perform a HeapSort
+            //check if heap is empty
             if (m_heap.size() <= 0)
                 return null;
             else {
-                ProcessNode topProcess = m_heap.get(0);
-                m_heap.set(0, m_heap.get(m_heap.size()-1));         // Move last to position 0
-                m_heap.remove(m_heap.size()-1);              // Removes copy at the bottom of the heap
+                ProcessNode topProcess = m_heap.get(0);             //Saves copy of topProcess
+                m_heap.set(0, m_heap.get(m_heap.size()-1));         // Move last to position 0 to prepare heapify function.
+                m_heap.remove(m_heap.size()-1);              // Deletes copy of last process at the bottom of the heap
                 heapify(0);
-                checkHeap();
+                debugPrintHeap();                                       // Prints heap to check for correctness.
                 return topProcess;
             }
         }
